716f84e44dc95df0e46d8f67db003c8a331773a2
Meshack-pi <kipronomeshack36@gmail.com>
2022-05-31 17:30:21 +0000

    all

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import { ChatEngine } from 'react-chat-engine';
 import './App.css';
 
 const App = () => {
-    return(
+    return (
         <ChatEngine
             height = "100vh"
-            projectID = "d3ff0cc6-fdf9-4a59-a5e5-023b3b05c386"
-            userName = "testuser"
+            projectID = "847617d3-f5b4-4507-b5e9-8a82830e2dc3"
+            userName = "Meshack"
             userSecret = "1234"
 
         />
-    )
+    );
 }
 
 export default App;
